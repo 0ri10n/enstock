761392141966dfbf4ce55513fdd45fd709efa403
Kevin Eduardo <kevineduardop614@gmail.com>
2026-09-21 17:22:16 -0600

    chore: generar primera migracion de base de datos en postgresql

---
+++ prisma/migrations/20260921231438_init/migration.sql
@@ -0,0 +1,55 @@
+-- CreateEnum
+CREATE TYPE "Rol" AS ENUM ('ADMIN', 'EMPLEADO');
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" TEXT NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "rol" "Rol" NOT NULL DEFAULT 'EMPLEADO',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Categoria" (
+    "id" TEXT NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Categoria_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Producto" (
+    "id" TEXT NOT NULL,
+    "sku" TEXT NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "categoriaId" TEXT NOT NULL,
+    "unidadMedida" TEXT NOT NULL,
+    "precioCompra" DECIMAL(65,30) NOT NULL,
+    "precioVenta" DECIMAL(65,30) NOT NULL,
+    "stockActual" DECIMAL(65,30) NOT NULL,
+    "stockMinimo" DECIMAL(65,30) NOT NULL,
+    "esPerecedero" BOOLEAN NOT NULL DEFAULT false,
+    "fechaCaducidad" TIMESTAMP(3),
+    "imagenUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Producto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Categoria_nombre_key" ON "Categoria"("nombre");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Producto_sku_key" ON "Producto"("sku");
+
+-- AddForeignKey
+ALTER TABLE "Producto" ADD CONSTRAINT "Producto_categoriaId_fkey" FOREIGN KEY ("categoriaId") REFERENCES "Categoria"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
